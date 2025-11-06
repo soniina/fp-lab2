0236cc3bad812963e5eff5f7f78081b282b762c0
soniina <sonikpav@gmail.com>
2025-11-06 19:56:40 +0300

    test: add unit tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,42 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Data.List (sort)
+import RBMultiset
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck as QC
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain tests
+
+tests :: TestTree
+tests =
+  testGroup
+    "RBMultiset Tests"
+    [unitTests]
+
+unitTests :: TestTree
+unitTests =
+  testGroup
+    "Unit Tests"
+    [ testCase "mempty tree toList is []" $
+        toList (mempty :: RBNode Int) @?= [],
+      testCase "insertRB and fromList" $
+        toList (fromList [3, 1, 2, 1 :: Int]) @?= [1, 1, 2, 3],
+      testCase "deleteRB removes one occurrence" $
+        toList (deleteRB 2 (fromList [3, 1, 2, 1, 1 :: Int])) @?= [1, 1, 1, 3],
+      testCase "deleteRB removes multiple occurrences" $
+        toList (deleteRB 1 (fromList [3, 1, 2, 1, 1 :: Int])) @?= [1, 1, 2, 3],
+      testCase "filterRB keeps even elements" $
+        toList (filterRB even (fromList [1, 2, 3, 4, 5 :: Int])) @?= [2, 4],
+      testCase "mapRB doubles elements" $
+        toList (mapRB (* 2) (fromList [1, 2, 3 :: Int])) @?= [2, 4, 6],
+      testCase "foldlRB divides elements" $
+        foldlRB div 16 (fromList [1, 2, 4 :: Int]) @?= 2,
+      testCase "foldrRB sums elements" $
+        foldrRB (+) 0 (fromList [1, 2, 3, 4 :: Int]) @?= 10,
+      testCase "Eq: same multiset (different order)" $
+        fromList [10, 5, 15, 3, 7, 3 :: Int] @=? fromList [5, 3, 7, 10, 15, 3],
+      testCase "Eq: different multisets" $
+        False @=? (fromList [1, 2 :: Int] == fromList [1, 1])
+    ]
